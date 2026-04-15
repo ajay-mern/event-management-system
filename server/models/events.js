81f7204f225c8ed2e44092af453c5b36af3f91de
ajay-mern <anasaramajay21@gmail.com>
2026-04-15 13:22:32 +0530

    added loaction field to event model

--- server/models/events.js
+++ server/models/events.js
@@ -4,6 +4,7 @@ const eventSchema = new mongoose.Schema({
     description: { type: String, required: true },
     category: { type: String, required: true },
     date: { type: Date, required: true },
+    location: { type: String, required: true },
     capacity: { type: Number, required: true },
     posterImage: { type: String, required: true },
     organizerID: { type: mongoose.Types.ObjectId, required: true },
